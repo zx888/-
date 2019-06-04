e830697706f45b4318c244f11d072793725c67cd
zx <shuhua141@163.com>
2019-06-04 20:47:52 +0800

    开启eslint相关

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: true, // 在保存代码的时候开启eslint代码检查机制
+  devServer: {
+    // 实时保存、编译的配置段
+    open: true, // 自动打开浏览器
+    port: 12306 // http服务的端口号码设定
+  }
 };
